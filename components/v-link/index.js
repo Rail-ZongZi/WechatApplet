1a649d7f4b49d655d474e825f4bc430e44eea10a
粽子 <rail_0039@163.com>
2020-03-20 21:41:53 +0800

    点赞组件编写

---
+++ components/v-link/index.js
@@ -0,0 +1,26 @@
+// components/v-link/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    linkText: 889,
+    linkImg: true,
+    yesLinkImg: "./images/link.png",
+    noLinkImg: "./images/link@dis.png"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
